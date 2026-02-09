cbb98d0d8f0375046760c95fbca0e1118c631f49
louisthecat <louiskatze1@web.de>
2026-02-09 17:41:36 +0000

    Fix ClassNotFoundException by correcting directory structure

---
+++ android/app/src/main/kotlin/com/example/einundzwanzig_meetup_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.einundzwanzig_meetup_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/de/einundzwanzig/einundzwanzig_meetup_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package de.einundzwanzig.einundzwanzig_meetup_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
